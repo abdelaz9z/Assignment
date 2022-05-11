e9e423f14cd484c0bdb478949f53044296f7aa8f
Abdelaziz Daoud <abdelaz9z@icloud.com>
2022-05-11 20:18:34 +0200

    Categorizing data according to (video or PDF) and re-showing it in RecyclerView

--- app/src/main/java/com/hyperone/assignment/ui/main/MainViewModel.kt
+++ app/src/main/java/com/hyperone/assignment/ui/main/MainViewModel.kt
@@ -13,25 +13,18 @@ import com.hyperone.assignment.network.APIService
  *
  * @property _isLoading MutableLiveData<Boolean>
  * @property isLoading MutableLiveData<Boolean>
- * @property videoSources ArrayList<Source>
- * @property pdfSources ArrayList<Source>
- * @property _pdfList MutableLiveData<ArrayList<Source>>
- * @property pdfList LiveData<ArrayList<Source>>
- * @property _videoList MutableLiveData<ArrayList<Source>>
- * @property videoList LiveData<ArrayList<Source>>
+ * @property _sourceList MutableLiveData<ArrayList<Source>>
+ * @property sourceList LiveData<ArrayList<Source>>
  */
-class MainViewModel : ViewModel() {
+class MainViewModel() : ViewModel() {
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading = _isLoading
 
-    var videoSources: ArrayList<Source> = ArrayList()
-    var pdfSources: ArrayList<Source> = ArrayList()
-
     /**
      * Get the list of sources from the API
      */
-    private val _pdfList = MutableLiveData<ArrayList<Source>>().apply {
+    private val _sourceList = MutableLiveData<ArrayList<Source>>().apply {
 
         _isLoading.value = true
 
@@ -50,15 +43,7 @@ class MainViewModel : ViewModel() {
                 if (response.isSuccessful) {
                     _isLoading.value = false
                     val sources: List<Source?>? = response.body()
-
-                    for (source in sources!!) {
-                        if (source?.type == "PDF") {
-                            pdfSources.add(source)
-                        }
-                        value = pdfSources
-                    }
-
-
+                    value = sources as ArrayList<Source>
                 }
             }
         })
@@ -67,46 +52,6 @@ class MainViewModel : ViewModel() {
     /**
      * Get the list of sources
      */
-    val pdfList: LiveData<ArrayList<Source>> = _pdfList
+    val sourceList: LiveData<ArrayList<Source>> = _sourceList
 
-    /**
-     * Get the list of sources from the API
-     */
-    private val _videoList = MutableLiveData<ArrayList<Source>>().apply {
-
-        _isLoading.value = true
-
-        val retro = APIClient().getRetrofitClient().create(APIService::class.java)
-        retro.getSources().enqueue(object : retrofit2.Callback<List<Source>> {
-            override fun onFailure(call: retrofit2.Call<List<Source>>, throwable: Throwable) {
-                _isLoading.value = false
-                val message = throwable.message
-                message?.let { Log.e("TAG", it) }
-            }
-
-            override fun onResponse(
-                call: retrofit2.Call<List<Source>>,
-                response: retrofit2.Response<List<Source>>
-            ) {
-                if (response.isSuccessful) {
-                    _isLoading.value = false
-                    val sources: List<Source?>? = response.body()
-
-                    for (source in sources!!) {
-                        if (source?.type == "VIDEO") {
-                            videoSources.add(source)
-                        }
-                        value = videoSources
-                    }
-
-
-                }
-            }
-        })
-    }
-
-    /**
-     * Get the list of sources
-     */
-    val videoList: LiveData<ArrayList<Source>> = _videoList
 }
