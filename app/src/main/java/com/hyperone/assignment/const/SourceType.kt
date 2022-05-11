54e92006bdd7282fc6ff7e020263fb63ea8dcee9
Abdelaziz Daoud <abdelaz9z@icloud.com>
2022-05-11 23:04:07 +0200

    Improve the appearance of the application

--- app/src/main/java/com/hyperone/assignment/const/SourceType.kt
+++
@@ -1,9 +0,0 @@
-package com.hyperone.assignment.const
-
-/**
- * SourceType enum class - Enum class for source type
- */
-object SourceType {
-    const val VIDEO = "VIDEO"
-    const val PDF = "PDF"
-}
